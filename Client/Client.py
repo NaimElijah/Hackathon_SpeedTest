1ebe4cbf29cd3fb06b6bb686e8f2c385ecde8460
eitandub22 <eitandu@post.bgu.ac.il>
2025-01-11 15:07:57 +0200

    client and server works with 1 TCP and 1 UDP connection

--- Client/Client.py
+++ Client/Client.py
@@ -1,95 +1,14 @@
-import threading, time, struct, sys
-from socket import *
-#*                ====================>>>>            Team Name:   "The United Front" / "The Speed Testing Association"            <<<<====================
+import threading, time, struct, sys, socket
 
-# global variables
-serverName = "servername"
-serverPort = 12000
-
-def main():
-    # ● Startup. You leave this state when you’re done asking the user for parameters.
-    # ask for the parameteres and store them in variables.
-    FileSize = input("Enter file size: ")
-    TCPConnCount = input("Choose amount of TCP  connections: ")
-    UDPConnCount = input("Enter amount of UDP connection: ")
-
-    if (FileSize != None) and (TCPConnCount != None) and (UDPConnCount != None):   # input checking, so that only if legit, we start the client.
-
-        #####################################################################################   UUUDP Client from lecture
-        # set up the listening UDP socket
-        UDPclientSocket = socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP
-        UDPclientSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)  # from assignment, for multiple clients to be able to listen from the same port
-        UDPclientSocket.bind(('',serverPort))
-
-        returning_Message, serverAddress = UDPclientSocket.recvfrom(2048)  # receive UDP offer from the server
-
-        message = "somethingggggggggggggggggg"  #TODO:  make the message into the packet needed     <<<=======================
-
-        UDPclientSocket.sendto(message, (serverName, serverPort))  # send  UDP
-
-        # while   -  receive the data until no more data sending, just a sketch
-        returning_Message, serverAddress = UDPclientSocket.recvfrom(2048)  # receive  UDP
-
-        #####################################################################################   UUUDP Client from lecture
-
-
-
-        #####################################################################################   TTTCP Client from lecture
-        TCPclientSocket = socket(AF_INET, SOCK_STREAM)
-        TCPclientSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)  # from assignment, for multiple clients to be able to listen from the same port
-        TCPclientSocket.bind(('',serverPort))
-        TCPclientSocket.connect((serverName, serverPort))
-        
-        sentence = "messageee"  #TODO:  a packet that needs to be sent, if something is needed to be sent because we only receive data from the server
-
-        TCPclientSocket.send(sentence)
-        modifiedMessage = TCPclientSocket.recv(1024)
-        # do something with modifiedMessage
-
-        #####################################################################################   TTTCP Client from lecture
-
-
-        print("Client started, listening for offer requests...")
-
-
-        # ● Looking for a server. You leave this state when you get an offer message.
-        #TODO: listen and receive a server's offer
-        
-
-        # ● Speed test. When you enter this state you should launch multiple threads, one for each
-        # TCP and UDP connection. You leave this state when all file transfers are finished. And then return to listening stage again.   <<===========  TCPThread and UDPThread are made.
-        #TODO: 
-
-        # end of client program, the threads run infinitly so we won't get here
-        UDPclientSocket.close()
-        TCPclientSocket.close()
-
-if __name__ == "__main__":
-    main()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#  a class way:
-
+#*                ====================>>>>            Team Name:  "The Speed Testing Association"            <<<<====================
 
 class SpeedTestClient:
+    BROADCAST_PORT = 13117
+    MAGIC_COOKIE = 0xabcddcba
+    TYPE_OFFER = 0x2
+    TYPE_REQUEST = 0x3
+    TYPE_PAYLOAD = 0x4
+
     def __init__(self):
         self.server_ip = None
         self.udp_port = None
@@ -102,66 +21,100 @@ class SpeedTestClient:
 
     def listen_for_offers(self):
         with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
-            sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-            sock.bind(("", 13117))  # Bind to UDP port 13117
+            sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)  # maybe REUSEPORT
+            # TODO: consider binding to all ports from 1024 to 65535
+            sock.bind(("", self.BROADCAST_PORT))  # Bind to UDP port 13117
             print("Client started, listening for offer requests...")
             
             while True:
                 data, addr = sock.recvfrom(1024)
                 magic_cookie, message_type, udp_port, tcp_port = struct.unpack("!IBHH", data)
-                if magic_cookie == 0xabcddcba and message_type == 0x2:
+                if magic_cookie == self.MAGIC_COOKIE and message_type == self.TYPE_OFFER:
                     self.server_ip = addr[0]
                     self.udp_port = udp_port
                     self.tcp_port = tcp_port
-                    print(f"Received offer from {self.server_ip}, UDP port: {self.udp_port}, TCP port: {self.tcp_port}")
+                    print(f"Received offer from {self.server_ip}, UDP port: {self.udp_port}, TCP port: {self.tcp_port}")   # TODO: make sure the prints in the assignment are as instructed
                     return
 
     def tcp_speed_test(self, connection_id):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
             sock.connect((self.server_ip, self.tcp_port))
-            sock.sendall(f"{self.file_size}\n".encode())
+            request_packet = struct.pack("!IBQ1s", self.MAGIC_COOKIE, self.TYPE_REQUEST, self.file_size, b"\n")
+            sock.sendall(request_packet)
             
-            start_time = time.time()
             bytes_received = 0
-            
-            while bytes_received < self.file_size:
-                data = sock.recv(4096)
-                if not data:
+            total_expected_segments = 1
+            current_segment = 0
+            start_time = time.time()
+
+            while current_segment < total_expected_segments:
+                print("receiving data")
+                data = sock.recv(self.file_size + 21)
+                print("data received")
+                (magic, msg_type, total_segments, current_received_segment) = struct.unpack('!IBQQ', data[0:21])
+
+                if magic != self.MAGIC_COOKIE or msg_type != self.TYPE_PAYLOAD:
+                    raise ValueError("Invalid Paylaod received - magic or type error")
+                     
+                if current_received_segment != current_segment + 1:
+                    raise ValueError("Invalid segment received, current_received_segment != current_segment + 1")
+                
+                current_segment = current_received_segment
+                
+                if not data[21:]:
                     break
-                bytes_received += len(data)
+                bytes_received += len(data[21:])
             
             elapsed_time = time.time() - start_time
-            speed = (bytes_received * 8) / elapsed_time  # Speed in bits/second
-            print(f"TCP transfer #{connection_id} finished, total time: {elapsed_time:.2f} seconds, total speed: {speed:.2f} bits/second")
+            if elapsed_time == 0:
+                speed = (bytes_received * 8)  # so we won't get division by 0 error
+            else:
+                speed = (bytes_received * 8) / elapsed_time  # Speed in bits/second
+            print(f"TCP transfer #{connection_id} finished, total time: {elapsed_time} seconds, total speed: {speed:.2f} bits/second, total bytes received: {bytes_received}")
+
 
     def udp_speed_test(self, connection_id):
         with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
-            request_packet = struct.pack("!IBQ", 0xabcddcba, 0x3, self.file_size)
+            request_packet = struct.pack("!IBQ", self.MAGIC_COOKIE, self.TYPE_REQUEST, self.file_size)
             sock.sendto(request_packet, (self.server_ip, self.udp_port))
             
             sock.settimeout(1.0)  # Timeout for UDP transfer
-            start_time = time.time()
             packets_received = 0
             packets_total = 0
+            total_expected_segments = 1
+            current_segment = 0
+            start_time = time.time()
 
             try:
-                while True:
-                    data, _ = sock.recvfrom(1024)
+                while current_segment < total_expected_segments:
+                    data, _ = sock.recvfrom(4096)  # generally, our server sends 1024+21 each packet
                     packets_total += 1
-                    magic_cookie, message_type, total_segments, current_segment = struct.unpack("!IBQQ", data[:21])
-                    if magic_cookie == 0xabcddcba and message_type == 0x4:
-                        packets_received += 1
+                    magic_cookie, message_type, total_segments, current_received_segment = struct.unpack("!IBQQ", data[:21])
+                    if magic_cookie != self.MAGIC_COOKIE or message_type != self.TYPE_PAYLOAD:
+                        raise ValueError("Invalid Paylaod received")
+                
+                    if total_expected_segments == 1:
+                        total_expected_segments = total_segments
+                    
+                    if current_received_segment == current_segment + 1:
+                        current_segment = current_received_segment
+                        if magic_cookie == self.MAGIC_COOKIE and message_type == self.TYPE_PAYLOAD:
+                            packets_received += 1
+                        
             except socket.timeout:
                 pass
             
             elapsed_time = time.time() - start_time
-            speed = (packets_received * 8 * 1024) / elapsed_time  # Speed in bits/second
+            if elapsed_time == 0:
+                speed = (packets_received * 8 * 1024)  # so we won't get division by 0 error
+            else:
+                speed = (packets_received * 8 * 1024) / elapsed_time  # Speed in bits/second
             packet_loss = 100 - (packets_received / packets_total * 100 if packets_total > 0 else 0)
             print(f"UDP transfer #{connection_id} finished, total time: {elapsed_time:.2f} seconds, total speed: {speed:.2f} bits/second, percentage of packets received successfully: {100 - packet_loss:.2f}%")
 
     def run_speed_test(self):
         threads = []
-        
+        print("Starting speed test...")
         for i in range(1, self.tcp_connections + 1):
             thread = threading.Thread(target=self.tcp_speed_test, args=(i,))
             threads.append(thread)
@@ -188,4 +141,11 @@ class SpeedTestClient:
 
 if __name__ == "__main__":
     client = SpeedTestClient()
-    client.run()
+    try:
+        client.run()
+    except KeyboardInterrupt:
+        print("\nKeyboardInterrupt - Exiting...\n")
+        sys.exit(0)
+    except ValueError:
+        print("\nValueError - Exiting...\n")
+        sys.exit(0)
--- Server/Server.py
+++ Server/Server.py
@@ -1,135 +1,62 @@
-import threading, time, struct, sys, random
-from socket import *
-#*                ====================>>>>            Team Name:   "The United Front" / "The Speed Testing Association"            <<<<====================
+import threading, time, struct, sys, random, socket, psutil
 
-# global variables
-serverName = "servername"
-serverPort = 12000
-BroadcastIP = "255.255.255.255"
-# setting up UDPserverSocket
-UDPserverSocket = socket(AF_INET, SOCK_DGRAM)  # UDP from lecture
-UDPserverSocket.bind(('',serverPort))  # UDP from lecture
-# setting up TCPserverSocket
-TCPserverSocket = socket(AF_INET, SOCK_STREAM)  # TCP from lecture
-TCPserverSocket.bind(("", serverPort))  # TCP from lecture
-
-def main():
-
-    # The server is also a multi-threaded app - it constantly sends offer messages in one thread
-    ListeningThread = threading.Thread(target=Request_Listening, args=())  #  see if args are needed here
-    OfferingThread = threading.Thread(target=offering, args=())  #  see if args are needed here
-    
-    ListeningThread.start()
-    OfferingThread.start()
-
-    # and launches a new thread to handle each incoming speed test request (either UDP or TCP).
-    ###############################################################################################   TTTCP
-    while True:
-        TCPserverSocket.listen(1) # put this line in the right place     # TCP from lecture
-        connectionSocket, addr = TCPserverSocket.accept()
-
-        sentence = connectionSocket.recv(1024)
-        Processed_Sentence = sentence.upper()  #TODO:  <<<=========  do something with the received packet and return an appropriate packet
-        connectionSocket.send(Processed_Sentence)
-        connectionSocket.close()  # this line probably needs to be somewhere else maybe
-
-    ###############################################################################################   TTTCP
-    #TODO: start a thread for each incoming request.
-
-
-    # end of main
-    OfferingThread.join()  # won't get here because the threads run infinitly
-
-
-
-if __name__ == "__main__":
-    main()
-
-
-
-def offering():   # send UDP offer every second, probably need to broadcast with address 255.255.255.255
-    while True:
-        # send offer
-        Offer_Packet  #TODO: make the offer_Packet accordingly   <<==================
-        UDPserverSocket.sendto(Offer_Packet, BroadcastIP)
-        time.sleep(1)
-
-
-def Request_Listening():  #  listening to UDP request
-    # create a server socket or something like that so we can hear through that
-    while True:   # always listen and create thread for each request heard
-        message, clientAddress = UDPserverSocket.recvfrom(2048)  # ListeningThread waiting for request and getting it when it arrives
-
-        # if request received, create a thread to take care of that request
-        # do something..
-        modifiedMessage = message.upper()  #TODO:   <<<<=================   do something with the message received and make the packet that is needed to be sent
-        UDPserverSocket.sendto(modifiedMessage, clientAddress)
-
-
-
-def thread_sending_data():
-    # make an array of the data parts to send and send each one using the current thread
-    return
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#  a class way:
-
+#*                ====================>>>>            Team Name:  "The Speed Testing Association"            <<<<====================
 
 class SpeedTestServer:
+    MAGIC_COOKIE = 0xabcddcba
+    TYPE_OFFER = 0x2
+    TYPE_REQUEST = 0x3
+    TYPE_PAYLOAD = 0x4
+
     def __init__(self):
         self.udp_port = random.randint(20000, 30000)
         self.tcp_port = random.randint(30001, 40000)
-        self.magic_cookie = 0xabcddcba
         self.running = True
+        self.broadcast_port = 13117
+
+
+    def get_ip_address(self, ifname):
+        for iface, addrs in psutil.net_if_addrs().items():
+            if iface == ifname:
+                for addr in addrs:
+                    if addr.family == socket.AF_INET:
+                        return addr.address
+        raise ValueError(f"No IPv4 address found for interface {ifname}")
+
 
     def send_offers(self):
         """Send periodic UDP offer messages to announce server availability."""
         with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
             sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-            print(f"Server started, listening on UDP port {self.udp_port}, TCP port {self.tcp_port}")
+            print(f"Server started, listening on Ip address {self.get_ip_address('Ethernet 4')}")
             
-            offer_packet = struct.pack("!IBHH", self.magic_cookie, 0x2, self.udp_port, self.tcp_port)
+            offer_packet = struct.pack("!IBHH", self.MAGIC_COOKIE, self.TYPE_OFFER, self.udp_port, self.tcp_port)
             
-            while self.running:
-                sock.sendto(offer_packet, ("<broadcast>", 13117))
+            while self.running: # TODO: consider sending offers in every port from 1024 to 65535
+                sock.sendto(offer_packet, ("<broadcast>", self.broadcast_port))
                 time.sleep(1)  # Send offer every second
+
 
     def handle_tcp_request(self, conn, addr):
         """Handle TCP file size requests."""
         try:
-            data = conn.recv(1024).decode().strip()
-            file_size = int(data)
+            data = conn.recv(1024)
+            (magic, msg_type, file_size, endline_char) = struct.unpack('!IBQ1s', data)
+            if magic != self.MAGIC_COOKIE or msg_type != self.TYPE_REQUEST or endline_char != b"\n":
+                raise ValueError("Invalid TCP request")
             print(f"TCP request from {addr}, file size: {file_size} bytes")
 
             # Simulate sending the file
-            bytes_sent = 0
-            while bytes_sent < file_size:
-                chunk_size = min(4096, file_size - bytes_sent)
-                conn.send(b"x" * chunk_size)
-                bytes_sent += chunk_size
-            
+            payload_msg = struct.pack("!IBQQ", self.MAGIC_COOKIE, self.TYPE_PAYLOAD, 1, 1) + b"x" * file_size # TODO: fix in client and server
+           
+            conn.send(payload_msg)
+
             print(f"TCP transfer to {addr} completed")
         except Exception as e:
             print(f"Error handling TCP request from {addr}: {e}")
         finally:
             conn.close()
+
 
     def handle_udp_request(self, sock, addr, file_size):
         """Handle UDP file size requests."""
@@ -138,34 +65,51 @@ class SpeedTestServer:
 
             # Simulate sending data in UDP packets
             total_segments = (file_size + 1023) // 1024
+            remaining_bytes = file_size
             for segment in range(total_segments):
-                packet = struct.pack("!IBQQ", self.magic_cookie, 0x4, total_segments, segment + 1) + b"x" * 1024
+                payload_size = min(remaining_bytes, 1024)
+                packet = struct.pack("!IBQQ", self.MAGIC_COOKIE, self.TYPE_PAYLOAD, total_segments, segment + 1) + b"x" * payload_size
+                remaining_bytes -= 1024
                 sock.sendto(packet, addr)
             
             print(f"UDP transfer to {addr} completed")
         except Exception as e:
             print(f"Error handling UDP request from {addr}: {e}")
 
+
     def listen_for_requests(self):
         """Listen for TCP and UDP client requests."""
+        print("Server listening for client requests...")
         # Start TCP listener
+        listenToTCPThread = threading.Thread(target=self.listen_for_TCP, args=())
+        # Start UDP listener
+        listenToUDPThread = threading.Thread(target=self.listen_for_UDP, args=())
+        
+        try:
+            listenToTCPThread.start()
+            listenToUDPThread.start()
+        except KeyboardInterrupt as e:
+            print(f"Error listening for requests: {e}")
+
+
+    def listen_for_TCP(self):
         tcp_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         tcp_sock.bind(("", self.tcp_port))
-        tcp_sock.listen()
-        
-        # Start UDP listener
+        while self.running:
+        # Handle TCP connections
+            tcp_sock.listen()
+            self.accept_tcp_requests(tcp_sock)
+    
+    
+    def listen_for_UDP(self):
         udp_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         udp_sock.bind(("", self.udp_port))
-
-        print("Server listening for client requests...")
         while self.running:
-            # Handle TCP connections
-            tcp_thread = threading.Thread(target=self.accept_tcp_requests, args=(tcp_sock,))
-            tcp_thread.start()
-
             # Handle UDP requests
-            data, addr = udp_sock.recvfrom(1024)
+            data, addr = udp_sock.recvfrom(4096)
             self.process_udp_request(data, addr, udp_sock)
+
+
 
     def accept_tcp_requests(self, tcp_sock):
         """Accept and handle TCP requests."""
@@ -173,15 +117,17 @@ class SpeedTestServer:
         thread = threading.Thread(target=self.handle_tcp_request, args=(conn, addr))
         thread.start()
 
+
     def process_udp_request(self, data, addr, udp_sock):
         """Process incoming UDP requests."""
         try:
             magic_cookie, message_type, file_size = struct.unpack("!IBQ", data[:13])
-            if magic_cookie == self.magic_cookie and message_type == 0x3:
+            if magic_cookie == self.MAGIC_COOKIE and message_type == self.TYPE_REQUEST:
                 thread = threading.Thread(target=self.handle_udp_request, args=(udp_sock, addr, file_size))
                 thread.start()
         except Exception as e:
             print(f"Invalid UDP request from {addr}: {e}")
+
 
     def run(self):
         """Run the server."""
@@ -192,12 +138,10 @@ class SpeedTestServer:
 
 
 
-
-
 if __name__ == "__main__":
     server = SpeedTestServer()
     try:
         server.run()
     except KeyboardInterrupt:
         server.running = False
-        print("Server shutting down...")
+        print("\nServer shutting down...\n")
