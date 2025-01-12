ae21178eda310120007ef11ecc99dc2683ea9b4d
eitandub22 <eitandu@post.bgu.ac.il>
2025-01-12 22:22:32 +0200

    added coloring and synchronization

--- Client/Client.py
+++ Client/Client.py
@@ -1,6 +1,9 @@
-import threading, time, struct, sys, socket
+import threading, time, struct, sys, socket, os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from Colors import bcolors
 
-#*                ====================>>>>            Team Name:  "The Speed Testing Association"            <<<<====================
+#*                    ====================>>>>            Team Name:  "Ping Floyd", "ACK/DC"               <<<<====================
+stdout_lock = threading.Lock()
 
 class SpeedTestClient:
     BROADCAST_PORT = 13117
@@ -14,17 +17,21 @@ class SpeedTestClient:
         self.udp_port = None
         self.tcp_port = None
 
+    def print_safe(self, message):
+        with stdout_lock:
+            print(message)
+
     def get_user_input(self):
-        self.file_size = int(input("Enter file size in bytes: "))
-        self.tcp_connections = int(input("Enter the number of TCP connections: "))
-        self.udp_connections = int(input("Enter the number of UDP connections: "))
+        self.file_size = int(input(f"{bcolors.YELLOW}Enter file size in bytes: {bcolors.ENDC}"))
+        self.tcp_connections = int(input(f"{bcolors.YELLOW}Enter the number of TCP connections: {bcolors.ENDC}"))
+        self.udp_connections = int(input(f"{bcolors.YELLOW}Enter the number of UDP connections: {bcolors.ENDC}"))
 
     def listen_for_offers(self):
         with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
             sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)  # maybe REUSEPORT
             # TODO: consider binding to all ports from 1024 to 65535
             sock.bind(("", self.BROADCAST_PORT))  # Bind to UDP port 13117
-            print("Client started, listening for offer requests...")
+            print(f"{bcolors.BLUE}Client started, listening for offer requests..{bcolors.ENDC}.")
             
             while True:
                 data, addr = sock.recvfrom(1024)
@@ -33,7 +40,7 @@ class SpeedTestClient:
                     self.server_ip = addr[0]
                     self.udp_port = udp_port
                     self.tcp_port = tcp_port
-                    print(f"Received offer from {self.server_ip}, UDP port: {self.udp_port}, TCP port: {self.tcp_port}")   # TODO: make sure the prints in the assignment are as instructed
+                    print(f"{bcolors.GREEN}Received offer from {self.server_ip}, UDP port: {self.udp_port}, TCP port: {self.tcp_port} {bcolors.ENDC}")  # TODO: make sure the prints in the assignment are as instructed
                     return
 
     def tcp_speed_test(self, connection_id):
@@ -48,16 +55,16 @@ class SpeedTestClient:
             start_time = time.time()
 
             while current_segment < total_expected_segments:
-                print("receiving data")
+                self.print_safe(f"{bcolors.BLUE}receiving data{bcolors.ENDC}")
                 data = sock.recv(self.file_size + 21)
-                print("data received")
+                self.print_safe(f"{bcolors.BLUE}data received{bcolors.ENDC}")
                 (magic, msg_type, total_segments, current_received_segment) = struct.unpack('!IBQQ', data[0:21])
 
                 if magic != self.MAGIC_COOKIE or msg_type != self.TYPE_PAYLOAD:
-                    raise ValueError("Invalid Paylaod received - magic or type error")
+                    raise ValueError(f"{bcolors.UNDERLINE}{bcolors.RED}{bcolors.BOLD}Invalid Paylaod received - magic or type error{bcolors.ENDC}")
                      
                 if current_received_segment != current_segment + 1:
-                    raise ValueError("Invalid segment received, current_received_segment != current_segment + 1")
+                    raise ValueError(f"{bcolors.UNDERLINE}{bcolors.RED}{bcolors.BOLD}Invalid segment received, current_received_segment != current_segment + 1{bcolors.ENDC}")
                 
                 current_segment = current_received_segment
                 
@@ -70,7 +77,7 @@ class SpeedTestClient:
                 speed = (bytes_received * 8)  # so we won't get division by 0 error
             else:
                 speed = (bytes_received * 8) / elapsed_time  # Speed in bits/second
-            print(f"TCP transfer #{connection_id} finished, total time: {elapsed_time} seconds, total speed: {speed:.2f} bits/second, total bytes received: {bytes_received}")
+            self.print_safe(f"{bcolors.GREEN}TCP transfer #{connection_id} finished, total time: {elapsed_time} seconds, total speed: {speed:.2f} bits/second, total bytes received: {bytes_received}{bcolors.ENDC}")
 
 
     def udp_speed_test(self, connection_id):
@@ -91,7 +98,7 @@ class SpeedTestClient:
                     packets_total += 1
                     magic_cookie, message_type, total_segments, current_received_segment = struct.unpack("!IBQQ", data[:21])
                     if magic_cookie != self.MAGIC_COOKIE or message_type != self.TYPE_PAYLOAD:
-                        raise ValueError("Invalid Paylaod received")
+                        raise ValueError(f"{bcolors.UNDERLINE}{bcolors.RED}{bcolors.BOLD}Invalid Paylaod received{bcolors.ENDC}")
                 
                     if total_expected_segments == 1:
                         total_expected_segments = total_segments
@@ -110,11 +117,11 @@ class SpeedTestClient:
             else:
                 speed = (packets_received * 8 * 1024) / elapsed_time  # Speed in bits/second
             packet_loss = 100 - (packets_received / packets_total * 100 if packets_total > 0 else 0)
-            print(f"UDP transfer #{connection_id} finished, total time: {elapsed_time:.2f} seconds, total speed: {speed:.2f} bits/second, percentage of packets received successfully: {100 - packet_loss:.2f}%")
+            self.print_safe(f"{bcolors.GREEN}UDP transfer #{connection_id} finished, total time: {elapsed_time:.2f} seconds, total speed: {speed:.2f} bits/second, percentage of packets received successfully: {100 - packet_loss:.2f}%{bcolors.ENDC}")
 
     def run_speed_test(self):
         threads = []
-        print("Starting speed test...")
+        print(f"{bcolors.BLUE}Starting speed test...{bcolors.ENDC}")
         for i in range(1, self.tcp_connections + 1):
             thread = threading.Thread(target=self.tcp_speed_test, args=(i,))
             threads.append(thread)
@@ -127,7 +134,7 @@ class SpeedTestClient:
 
         for thread in threads:
             thread.join()
-        print("All transfers complete, listening to offer requests")
+        print(f"{bcolors.UNDERLINE}{bcolors.BLUE}{bcolors.BOLD}All transfers complete, listening to offer requests{bcolors.ENDC}")
 
     def run(self):
         self.get_user_input()
@@ -144,8 +151,8 @@ if __name__ == "__main__":
     try:
         client.run()
     except KeyboardInterrupt:
-        print("\nKeyboardInterrupt - Exiting...\n")
+        print(f"\n{bcolors.BG_BLUE}{bcolors.BOLD}KeyboardInterrupt - Exiting...{bcolors.ENDC}\n")
         sys.exit(0)
     except ValueError:
-        print("\nValueError - Exiting...\n")
+        print(f"\n{bcolors.RED}{bcolors.BOLD}{bcolors.UNDERLINE}ValueError - Exiting...{bcolors.ENDC}\n")
         sys.exit(0)
--- Server/Server.py
+++ Server/Server.py
@@ -1,8 +1,9 @@
 import threading, time, struct, sys, random, socket, psutil, os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from colors import bcolors
+from Colors import bcolors
 
-#*                ====================>>>>            Team Name:  "The Speed Testing Association"            <<<<====================
+#*                    ====================>>>>            Team Name:  "Ping Floyd", "ACK/DC"               <<<<====================
+stdout_lock = threading.Lock()
 
 class SpeedTestServer:
     MAGIC_COOKIE = 0xabcddcba
@@ -15,7 +16,11 @@ class SpeedTestServer:
         self.tcp_port = random.randint(30001, 40000)
         self.running = True
         self.broadcast_port = 13117
+        self.udp_lock = threading.Lock()
 
+    def print_safe(self, message):
+        with stdout_lock:
+            print(message)
 
     def get_ip_address(self, ifname):
         for iface, addrs in psutil.net_if_addrs().items():
@@ -23,18 +28,18 @@ class SpeedTestServer:
                 for addr in addrs:
                     if addr.family == socket.AF_INET:
                         return addr.address
-        raise ValueError(f"No IPv4 address found for interface {ifname}")
+        raise ValueError(f"{bcolors.UNDERLINE}{bcolors.RED}{bcolors.BOLD}No IPv4 address found for interface {ifname}{bcolors.ENDC}")
 
 
     def send_offers(self):
         """Send periodic UDP offer messages to announce server availability."""
         with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
             sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-            print(f"Server started, listening on Ip address {self.get_ip_address('Ethernet 4')}")
+            self.print_safe(f"{bcolors.BLUE}Server started, listening on Ip address {self.get_ip_address('Ethernet 4')} {bcolors.ENDC}")
             
             offer_packet = struct.pack("!IBHH", self.MAGIC_COOKIE, self.TYPE_OFFER, self.udp_port, self.tcp_port)
             
-            while self.running: # TODO: consider sending offers in every port from 1024 to 65535
+            while self.running:
                 sock.sendto(offer_packet, ("<broadcast>", self.broadcast_port))
                 time.sleep(1)  # Send offer every second
 
@@ -45,17 +50,19 @@ class SpeedTestServer:
             data = conn.recv(1024)
             (magic, msg_type, file_size, endline_char) = struct.unpack('!IBQ1s', data)
             if magic != self.MAGIC_COOKIE or msg_type != self.TYPE_REQUEST or endline_char != b"\n":
-                raise ValueError("Invalid TCP request")
-            print(f"TCP request from {addr}, file size: {file_size} bytes")
+                raise ValueError(f"{bcolors.UNDERLINE}{bcolors.RED}{bcolors.BOLD}Invalid TCP request{bcolors.ENDC}")
+            self.print_safe(f"{bcolors.GREEN}TCP request from {addr}, file size: {file_size} bytes{bcolors.ENDC}")
 
             # Simulate sending the file
             payload_msg = struct.pack("!IBQQ", self.MAGIC_COOKIE, self.TYPE_PAYLOAD, 1, 1) + b"x" * file_size # TODO: fix in client and server
            
             conn.send(payload_msg)
 
-            print(f"TCP transfer to {addr} completed")
+            self.print_safe(f"{bcolors.BLUE}TCP transfer to {addr} completed{bcolors.ENDC}")
+        except (ConnectionResetError, socket.error) as e:
+            self.print_safe(f"{bcolors.UNDERLINE}{bcolors.RED}{bcolors.BOLD}Client {addr} disconnected: {e}{bcolors.ENDC}")
         except Exception as e:
-            print(f"Error handling TCP request from {addr}: {e}")
+            self.print_safe(f"{bcolors.UNDERLINE}{bcolors.RED}{bcolors.BOLD}Error handling TCP request from {addr}: {e}{bcolors.ENDC}")
         finally:
             conn.close()
 
@@ -63,25 +70,29 @@ class SpeedTestServer:
     def handle_udp_request(self, sock, addr, file_size):
         """Handle UDP file size requests."""
         try:
-            print(f"UDP request from {addr}, file size: {file_size} bytes")
+            self.print_safe(f"{bcolors.GREEN}UDP request from {addr}, file size: {file_size} bytes{bcolors.ENDC}")
 
             # Simulate sending data in UDP packets
             total_segments = (file_size + 1023) // 1024
             remaining_bytes = file_size
+
             for segment in range(total_segments):
                 payload_size = min(remaining_bytes, 1024)
                 packet = struct.pack("!IBQQ", self.MAGIC_COOKIE, self.TYPE_PAYLOAD, total_segments, segment + 1) + b"x" * payload_size
                 remaining_bytes -= 1024
-                sock.sendto(packet, addr)
+                with self.udp_lock:
+                    sock.sendto(packet, addr)
             
-            print(f"UDP transfer to {addr} completed")
+            self.print_safe(f"{bcolors.BLUE}UDP transfer to {addr} completed{bcolors.ENDC}")
+        except (ConnectionResetError, socket.error) as e:
+            self.print_safe(f"Client {addr} disconnected: {e}")
         except Exception as e:
-            print(f"Error handling UDP request from {addr}: {e}")
+            self.print_safe(f"{bcolors.UNDERLINE}{bcolors.RED}{bcolors.BOLD}Error handling UDP request from {addr}: {e}{bcolors.ENDC}")
 
 
     def listen_for_requests(self):
         """Listen for TCP and UDP client requests."""
-        print("Server listening for client requests...")
+        self.print_safe(f"{bcolors.BLUE}Server listening for client requests...{bcolors.ENDC}")
         # Start TCP listener
         listenToTCPThread = threading.Thread(target=self.listen_for_TCP, args=())
         # Start UDP listener
@@ -91,7 +102,7 @@ class SpeedTestServer:
             listenToTCPThread.start()
             listenToUDPThread.start()
         except KeyboardInterrupt as e:
-            print(f"Error listening for requests: {e}")
+            self.print_safe(f"{bcolors.UNDERLINE}{bcolors.RED}{bcolors.BOLD}Error listening for requests: {e}{bcolors.ENDC}")
 
 
     def listen_for_TCP(self):
@@ -128,7 +139,7 @@ class SpeedTestServer:
                 thread = threading.Thread(target=self.handle_udp_request, args=(udp_sock, addr, file_size))
                 thread.start()
         except Exception as e:
-            print(f"Invalid UDP request from {addr}: {e}")
+            self.print_safe(f"{bcolors.RED}{bcolors.BOLD}{bcolors.UNDERLINE}Invalid UDP request from {addr}: {e}{bcolors.ENDC}")
 
 
     def run(self):
@@ -141,10 +152,9 @@ class SpeedTestServer:
 
 
 if __name__ == "__main__":
-    print(f"{bcolors.OKBLUE}Server is starting...{bcolors.ENDC}")     #   <<<================================== tteestt
     server = SpeedTestServer()
     try:
         server.run()
     except KeyboardInterrupt:
         server.running = False
-        print("\nServer shutting down...\n")
+        print(f"\n{bcolors.BG_BLUE}{bcolors.BOLD}Server shutting down...{bcolors.ENDC}\n")
