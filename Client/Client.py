befe801ce72e1e63a1b2ccdc74fdfc2e456f7846
eitandub22 <eitandu@post.bgu.ac.il>
2025-01-12 22:29:17 +0200

    added error handling

--- Client/Client.py
+++ Client/Client.py
@@ -34,90 +34,100 @@ class SpeedTestClient:
             print(f"{bcolors.BLUE}Client started, listening for offer requests..{bcolors.ENDC}.")
             
             while True:
-                data, addr = sock.recvfrom(1024)
-                magic_cookie, message_type, udp_port, tcp_port = struct.unpack("!IBHH", data)
-                if magic_cookie == self.MAGIC_COOKIE and message_type == self.TYPE_OFFER:
-                    self.server_ip = addr[0]
-                    self.udp_port = udp_port
-                    self.tcp_port = tcp_port
-                    print(f"{bcolors.GREEN}Received offer from {self.server_ip}, UDP port: {self.udp_port}, TCP port: {self.tcp_port} {bcolors.ENDC}")  # TODO: make sure the prints in the assignment are as instructed
-                    return
+                try:
+                    data, addr = sock.recvfrom(1024)
+                    magic_cookie, message_type, udp_port, tcp_port = struct.unpack("!IBHH", data)
+                    if magic_cookie == self.MAGIC_COOKIE and message_type == self.TYPE_OFFER:
+                        self.server_ip = addr[0]
+                        self.udp_port = udp_port
+                        self.tcp_port = tcp_port
+                        print(f"{bcolors.GREEN}Received offer from {self.server_ip}, UDP port: {self.udp_port}, TCP port: {self.tcp_port} {bcolors.ENDC}")  # TODO: make sure the prints in the assignment are as instructed
+                        return
+                except (ConnectionResetError, socket.error) as e:
+                    self.print_safe(f"{bcolors.RED}{bcolors.BOLD}{bcolors.UNDERLINE}Error receiving offer: {e}{bcolors.ENDC}")
 
     def tcp_speed_test(self, connection_id):
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-            sock.connect((self.server_ip, self.tcp_port))
-            request_packet = struct.pack("!IBQ1s", self.MAGIC_COOKIE, self.TYPE_REQUEST, self.file_size, b"\n")
-            sock.sendall(request_packet)
-            
-            bytes_received = 0
-            total_expected_segments = 1
-            current_segment = 0
-            start_time = time.time()
-
-            while current_segment < total_expected_segments:
-                self.print_safe(f"{bcolors.BLUE}receiving data{bcolors.ENDC}")
-                data = sock.recv(self.file_size + 21)
-                self.print_safe(f"{bcolors.BLUE}data received{bcolors.ENDC}")
-                (magic, msg_type, total_segments, current_received_segment) = struct.unpack('!IBQQ', data[0:21])
-
-                if magic != self.MAGIC_COOKIE or msg_type != self.TYPE_PAYLOAD:
-                    raise ValueError(f"{bcolors.UNDERLINE}{bcolors.RED}{bcolors.BOLD}Invalid Paylaod received - magic or type error{bcolors.ENDC}")
-                     
-                if current_received_segment != current_segment + 1:
-                    raise ValueError(f"{bcolors.UNDERLINE}{bcolors.RED}{bcolors.BOLD}Invalid segment received, current_received_segment != current_segment + 1{bcolors.ENDC}")
+        try:
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+                sock.connect((self.server_ip, self.tcp_port))
+                request_packet = struct.pack("!IBQ1s", self.MAGIC_COOKIE, self.TYPE_REQUEST, self.file_size, b"\n")
+                sock.sendall(request_packet)
                 
-                current_segment = current_received_segment
+                bytes_received = 0
+                total_expected_segments = 1
+                current_segment = 0
+                start_time = time.time()
+
+                while current_segment < total_expected_segments:
+                    self.print_safe(f"{bcolors.BLUE}receiving data{bcolors.ENDC}")
+                    data = sock.recv(self.file_size + 21)
+                    self.print_safe(f"{bcolors.BLUE}data received{bcolors.ENDC}")
+                    (magic, msg_type, total_segments, current_received_segment) = struct.unpack('!IBQQ', data[0:21])
+
+                    if magic != self.MAGIC_COOKIE or msg_type != self.TYPE_PAYLOAD:
+                        raise ValueError(f"{bcolors.UNDERLINE}{bcolors.RED}{bcolors.BOLD}Invalid Paylaod received - magic or type error{bcolors.ENDC}")
+                        
+                    if current_received_segment != current_segment + 1:
+                        raise ValueError(f"{bcolors.UNDERLINE}{bcolors.RED}{bcolors.BOLD}Invalid segment received, current_received_segment != current_segment + 1{bcolors.ENDC}")
+                    
+                    current_segment = current_received_segment
+                    
+                    if not data[21:]:
+                        break
+                    bytes_received += len(data[21:])
                 
-                if not data[21:]:
-                    break
-                bytes_received += len(data[21:])
-            
-            elapsed_time = time.time() - start_time
-            if elapsed_time == 0:
-                speed = (bytes_received * 8)  # so we won't get division by 0 error
-            else:
-                speed = (bytes_received * 8) / elapsed_time  # Speed in bits/second
-            self.print_safe(f"{bcolors.GREEN}TCP transfer #{connection_id} finished, total time: {elapsed_time} seconds, total speed: {speed:.2f} bits/second, total bytes received: {bytes_received}{bcolors.ENDC}")
+                elapsed_time = time.time() - start_time
+                if elapsed_time == 0:
+                    speed = (bytes_received * 8)  # so we won't get division by 0 error
+                else:
+                    speed = (bytes_received * 8) / elapsed_time  # Speed in bits/second
+                self.print_safe(f"{bcolors.GREEN}TCP transfer #{connection_id} finished, total time: {elapsed_time} seconds, total speed: {speed:.2f} bits/second, total bytes received: {bytes_received}{bcolors.ENDC}")
+        except (ConnectionResetError, socket.error) as e:
+            self.print_safe(f"{bcolors.RED}{bcolors.BOLD}{bcolors.UNDERLINE}Error connecting to server: {e}{bcolors.ENDC}")
+
 
 
     def udp_speed_test(self, connection_id):
-        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
-            request_packet = struct.pack("!IBQ", self.MAGIC_COOKIE, self.TYPE_REQUEST, self.file_size)
-            sock.sendto(request_packet, (self.server_ip, self.udp_port))
-            
-            sock.settimeout(1.0)  # Timeout for UDP transfer
-            packets_received = 0
-            packets_total = 0
-            total_expected_segments = 1
-            current_segment = 0
-            start_time = time.time()
-
-            try:
-                while current_segment < total_expected_segments:
-                    data, _ = sock.recvfrom(4096)  # generally, our server sends 1024+21 each packet
-                    packets_total += 1
-                    magic_cookie, message_type, total_segments, current_received_segment = struct.unpack("!IBQQ", data[:21])
-                    if magic_cookie != self.MAGIC_COOKIE or message_type != self.TYPE_PAYLOAD:
-                        raise ValueError(f"{bcolors.UNDERLINE}{bcolors.RED}{bcolors.BOLD}Invalid Paylaod received{bcolors.ENDC}")
+        try:
+            with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
+                request_packet = struct.pack("!IBQ", self.MAGIC_COOKIE, self.TYPE_REQUEST, self.file_size)
+                sock.sendto(request_packet, (self.server_ip, self.udp_port))
                 
-                    if total_expected_segments == 1:
-                        total_expected_segments = total_segments
+                sock.settimeout(1.0)  # Timeout for UDP transfer
+                packets_received = 0
+                packets_total = 0
+                total_expected_segments = 1
+                current_segment = 0
+                start_time = time.time()
+
+                try:
+                    while current_segment < total_expected_segments:
+                        data, _ = sock.recvfrom(4096)  # generally, our server sends 1024+21 each packet
+                        packets_total += 1
+                        magic_cookie, message_type, total_segments, current_received_segment = struct.unpack("!IBQQ", data[:21])
+                        if magic_cookie != self.MAGIC_COOKIE or message_type != self.TYPE_PAYLOAD:
+                            raise ValueError(f"{bcolors.UNDERLINE}{bcolors.RED}{bcolors.BOLD}Invalid Paylaod received{bcolors.ENDC}")
                     
-                    if current_received_segment == current_segment + 1:
-                        current_segment = current_received_segment
-                        if magic_cookie == self.MAGIC_COOKIE and message_type == self.TYPE_PAYLOAD:
-                            packets_received += 1
+                        if total_expected_segments == 1:
+                            total_expected_segments = total_segments
                         
-            except socket.timeout:
-                pass
-            
-            elapsed_time = time.time() - start_time
-            if elapsed_time == 0:
-                speed = (packets_received * 8 * 1024)  # so we won't get division by 0 error
-            else:
-                speed = (packets_received * 8 * 1024) / elapsed_time  # Speed in bits/second
-            packet_loss = 100 - (packets_received / packets_total * 100 if packets_total > 0 else 0)
-            self.print_safe(f"{bcolors.GREEN}UDP transfer #{connection_id} finished, total time: {elapsed_time:.2f} seconds, total speed: {speed:.2f} bits/second, percentage of packets received successfully: {100 - packet_loss:.2f}%{bcolors.ENDC}")
+                        if current_received_segment == current_segment + 1:
+                            current_segment = current_received_segment
+                            if magic_cookie == self.MAGIC_COOKIE and message_type == self.TYPE_PAYLOAD:
+                                packets_received += 1
+                            
+                except socket.timeout:
+                    pass
+                
+                elapsed_time = time.time() - start_time
+                if elapsed_time == 0:
+                    speed = (packets_received * 8 * 1024)  # so we won't get division by 0 error
+                else:
+                    speed = (packets_received * 8 * 1024) / elapsed_time  # Speed in bits/second
+                packet_loss = 100 - (packets_received / packets_total * 100 if packets_total > 0 else 0)
+                self.print_safe(f"{bcolors.GREEN}UDP transfer #{connection_id} finished, total time: {elapsed_time:.2f} seconds, total speed: {speed:.2f} bits/second, percentage of packets received successfully: {100 - packet_loss:.2f}%{bcolors.ENDC}")
+        except (ConnectionResetError, socket.error) as e:
+            self.print_safe(f"{bcolors.RED}{bcolors.BOLD}{bcolors.UNDERLINE}Error connecting to server: {e}{bcolors.ENDC}")
 
     def run_speed_test(self):
         threads = []
